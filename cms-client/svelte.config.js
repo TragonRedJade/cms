b8337425e9e4d7759f2a797dc0a642264003882d
Evan Lemke <evanlemke@gmail.com>
2023-05-11 14:53:24 -0600

    add tailwind

--- cms-client/svelte.config.js
+++ cms-client/svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-node';
 
@@ -8,7 +9,13 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
