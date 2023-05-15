4c2c29e5c5ca99b2b9659912550b75fcae930ebd
Ben Cipa <benjamincipa@hotmail.com>
2023-05-15 15:22:33 -0400

    Sample promise to grab posts

--- cms-client/src/routes/+page.svelte
+++ cms-client/src/routes/+page.svelte
@@ -1,33 +1,28 @@
-<script lang="ts" context="module">
-	import type { Load } from '@sveltejs/kit';
-	/** @type {import('./$types').PageData} */  
-
+<script>
 	import { goto } from '$app/navigation';
-	import defaultOptions from '../lib/utils/defaultOptions';
-	export const load: Load = async ({ fetch }) => {	
-		console.log("Hi");	
-		const res = await fetch('http://127.0.0.1:1337/api/posts', defaultOptions);
+
+	export async function loadPosts() {
+		const res = await fetch('http://localhost:1337/api/posts');
 		const response = await res.json();
-
-		return { props: { posts: response.data } };
-	};
-
-</script>
-<script lang="ts">
-	export let posts: any;
+		return response.data;
+	}
 </script>
 
 <div class="my-4">
 	<h1 class="text-center text-3xl font-bold">My wonderful blog</h1>
 </div>
 <div class="container mx-auto mt-4">
-	{posts}
-	<!-- {#each posts as post}
-		<div
-			class="hover:bg-gray-200 cursor-pointer px-6 py-2 border-b border-gray-500"
-			on:click={() => goto('/blog/' + post.id)}
-		> 
-			<h4 class="font-bold">{post.attributes.title}</h4>
-		</div>
-	{/each} -->
-	</div>
+	{#await loadPosts()}
+		<p>Loading...</p>
+	{:then posts}
+		{#each posts as post}
+			<div
+				class="hover:bg-gray-200 cursor-pointer px-6 py-2 border-b border-gray-500"
+				on:click={() => goto('/blog/' + post.id)}
+			>
+				<h4 class="font-bold">{post.attributes.Title}</h4>
+				<p class="mt-2 text-gray-800">{post.attributes.created_at}</p>
+			</div>
+		{/each}
+	{/await}
+</div>
