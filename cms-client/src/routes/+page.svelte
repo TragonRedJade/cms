bdc3dbc4c722260791d5872ef9f1f7cda504ac04
Ben Cipa <benjamincipa@hotmail.com>
2023-05-15 15:51:06 -0400

    Cleanup

--- cms-client/src/routes/+page.svelte
+++ cms-client/src/routes/+page.svelte
@@ -1,28 +1,22 @@
 <script>
-	import { goto } from '$app/navigation';
+	// EXAMPLE grab records from the database
 
-	export async function loadPosts() {
-		const res = await fetch('http://localhost:1337/api/posts');
-		const response = await res.json();
-		return response.data;
-	}
+	// export async function loadPosts() {
+	// 	const res = await fetch('http://localhost:1337/api/posts');
+	// 	const response = await res.json();
+	// 	return response.data;
+	// }
 </script>
 
-<div class="my-4">
-	<h1 class="text-center text-3xl font-bold">My wonderful blog</h1>
-</div>
 <div class="container mx-auto mt-4">
-	{#await loadPosts()}
+	<a href="/posts"> Posts </a>
+
+	<!-- EXAMPLE display records -->
+	<!-- {#await loadPosts()}
 		<p>Loading...</p>
 	{:then posts}
 		{#each posts as post}
-			<div
-				class="hover:bg-gray-200 cursor-pointer px-6 py-2 border-b border-gray-500"
-				on:click={() => goto('/blog/' + post.id)}
-			>
-				<h4 class="font-bold">{post.attributes.Title}</h4>
-				<p class="mt-2 text-gray-800">{post.attributes.created_at}</p>
-			</div>
+			<h4 class="font-bold">{post.attributes.Title}</h4>
 		{/each}
-	{/await}
+	{/await} -->
 </div>
--- cms-client/src/routes/posts/+page.ts
+++ cms-client/src/routes/posts/+page.ts
@@ -1,5 +1,6 @@
 export async function load({fetch}) {
     const res = await fetch('http://127.0.0.1:1337/api/posts');
     const response = await res.json();
+    
     return {posts: response.data};
 }
